0f07056303529ac5a0f8d00c9cff781200514d87
cheng-zheng <905618355@qq.com>
2018-05-31 14:26:39 +0800

    mailer map

---
+++ extend/Map.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/7/28
+ * Time: 16:37
+ * 百度地图相关业务封装
+ */
+class Map {
+    /*
+     * 根据地址来获取经纬度
+     * @param $address
+     * */
+    public static function getLngLat($address)
+    {
+        if(!$address){
+            return '';
+        }
+        //http://api.map.baidu.com/geocoder/v2/?address=北京市海淀区上地十街10号&output=json&ak=E4805d16520de693a3fe707cdc962045&callback=showLocation
+        $data = [
+            'address' => $address,
+            'ak'      => config('map.ak'),
+            'output'  => 'json',
+        ];
+        $url = config('map.baidu_map_url').config('map.geocoder').'?'.http_build_query($data);
+        //第一种方法 file_get_contents($url);
+        //第二种方法 curl
+        $result = doCurl($url);
+        return $result;
+    }
+    //http://api.map.baidu.com/staticimage/v2
+    /**
+     * 根据经纬度或者地址来获取百度地图
+     * @param $center
+     * @return array
+     */
+    public static function staticimage($center)
+    {
+        if(!$center){
+            return '';
+        }
+        $data = [
+            'ak'      => config('map.ak'),
+            'width'   => config('map.width'),
+            'height'  => config('map.height'),
+            'center'  => $center,
+            //'markers' => $center,
+        ];
+        $url = config('map.baidu_map_url').config('map.staticimage').'?'.http_build_query($data);
+        var_dump($url);
+        return $url;
+        //dump($url);
+        //第一种方法 file_get_contents($url);
+        //第二种方法 curl
+        $result = doCurl($url);
+        if($result){
+            return json_decode($result);
+        }else{
+            return [];
+        }
+        //dump($result);
+        //return $result;
+    }
+}
---
+++ extend/phpmailer/testemail.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 5/30/2018
+ * Time: 1:32 PM
+ */
+//header("content-type:text/html;charset=utf-8");
+/*require 'class.phpmailer.php';
+require 'class.smtp.php';*/
+/*require_once('class.phpmailer.php');
+require_once('class.smtp.php');
+date_default_timezone_set('PRC');//set time
+
+// Create a new PHPmailer instance
+$mail = new PHPMailer();
+
+$mail->isSMTP();
+
+$mail->SMTPDebug = 1;
+// 设置发送的邮件的编码
+$mail->CharSet = 'UTF-8';
+
+$mail->Debugoutput = 'html';
+
+$mail->Host = "smtp.qq.com";
+//设置使用ssl加密方式登录鉴权
+$mail->SMTPSecure = 'ssl';
+
+$mail->Port = 465;
+
+$mail->SMTPAuth = true;
+
+$mail->Username = "905618355@qq.com";
+
+$mail->Password = "hlyvlayqvpqrbaic";
+
+$mail->setFrom('905618355@qq.com','chen');
+
+$mail->addAddress('965950134@qq.com','tracy');
+
+$mail->Subject = "PHPMailer SMTP test";
+
+$mail->msgHTML('测试');*/
+//$mail->send();
+
+
