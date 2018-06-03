3c87e58e5be43843e8fac1c10426804807a80de5
cheng-zheng <905618355@qq.com>
2018-06-03 14:58:56 +0800

    up

--- application/route.php
+++ application/route.php
@@ -9,12 +9,14 @@
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
 
-return [
-    /*'__pattern__' => [
+/*return [
+    '__pattern__' => [
         'name' => '\w+',
     ],
     '[hello]'     => [
         ':id'   => ['index/hello', ['method' => 'get'], ['id' => '\d+']],
         ':name' => ['index/hello', ['method' => 'post']],
-    ]*/
-];
+    ]
+];*/
+
+Route::get('index/user/login','index/user/login'); 
